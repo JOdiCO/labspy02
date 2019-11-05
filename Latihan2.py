643822e2f35771cfcc663f0b4b07b0df18d8d32c
JOdiCO <guiltary81@gmail.com>
2019-11-05 14:34:31 +0700

    Latihan2

---
+++ Latihan2.py
@@ -0,0 +1,10 @@
+a = int(input("Masukkan bilangan A: "))
+b = int(input("Masukkan bilangan B: "))
+c = int(input("Masukkan bilangan C: "))
+
+if a>b and a>c:
+    print(a, "adalah bilangan terbesar")
+elif b>a and b>c:
+    print(b, "adalah bilangan tersebar")
+elif c>a and c>b:
+    print(c, "adalah bilangan terbesar")
